58f5cc4436ffeade732889d2190f1e3e721222eb
ROHAN <21a91a6126@aec.edu.in>
2024-01-05 16:49:04 +0530

    added LIS

---
+++ Longest_Increasing_Subsequence.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Longest_Increasing_Subsequence {
+    public static int lengthOfLIS(int[] nums) {
+        int temp[]=new int[nums.length];
+        int size=0;
+        for(int num : nums){
+            int i=0,j=size;
+            
+            while(i!=j){
+                int mid=(i+j)/2;
+                if(temp[mid]<num){
+                    i=mid+1;
+                }else{
+                    j=mid;
+                }
+            }
+            temp[i]=num;
+            if(i==size){
+                size++;
+            }
+        }
+        return size;
+        
+    }
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=sc.nextInt();
+        }
+        int ans=lengthOfLIS(a);
+        System.out.println(ans);
+    }
+}
